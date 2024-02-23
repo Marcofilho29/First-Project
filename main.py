7347a69edfc6a897198ab0f5b3b0817901d9c682
Marco Antonio <marco.filho29@yahoo.com.br>
2024-02-23 13:40:49 -0400

    eu primeiro commit

---
+++ main.py
@@ -0,0 +1,16 @@
+print("My first project")
+
+# variáveis
+idade = 35
+nome = "Marco"
+
+# print
+print(idade)
+
+# soma
+nova_idade = idade + 1
+print(nova_idade)
+
+# lista (i)
+lista = (1,2,3,4,5)
+
